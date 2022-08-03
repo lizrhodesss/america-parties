c1aa0af24e1694797421c9f20a860a6d67c1c49b
John Roy <johnroy71@gmail.com>
2022-08-03 17:10:02 -0500

    added comment form

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ fetch ('https://date.nager.at/api/v3/publicholidays/2022/US')
 .then ((holidays) => {
     renderHolidays(holidays)
     displayHoliday(holidays)
+    displayHoliday(holidays[0])
  })
 
 function renderHolidays(holidays) {
@@ -24,3 +25,17 @@ function displayHoliday(holidays) {
     const displayGlobal = document.querySelector('#global')
     displayGlobal.textContent = holidays.global
 }
+
+let form = document.querySelector('#holiday-form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const commentList = document.querySelector('#comments-list')
+        const commentHere = document.createElement("li")
+        newComment = document.querySelector("#holiday-comment").value
+        commentHere.innerText=newComment
+        console.log(commentHere)
+        commentList.append(commentHere)
+        form.reset()
+        
+}
+)
